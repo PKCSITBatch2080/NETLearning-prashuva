58f9a12f46453782e0caeaf06f14cf52977d1ecd
iamprashuva <prashuvadhital@gmail.com>
2024-02-20 07:53:15 +0545

    Learned collections

---
+++ FirstConsoleApp/Collection.cs
@@ -0,0 +1,17 @@
+class Coll {
+    void Learn(){
+        // One dimensional array
+        byte[] ages = [78, 23, 45, 60, 24];
+        string[] names = ["prashuva", "prena", ];
+        // Multi dimentional array
+        int[,] studentInClassRoom = new[7,6];
+        stuentInClassRoom[0,0]= 1;
+        stuentInClassRoom[0,1]= 6;
+        stuentInClassRoom[0,2]= 8;
+        stuentInClassRoom[0,3]= 13;
+
+        // jagged array
+        int[][] studentInClassRoom2 = new[7][];
+        int[][] studentInClassRoom3= [[1, 2, 3], [7, 5, 8, 10],[8, 13]];
+    }
+}
