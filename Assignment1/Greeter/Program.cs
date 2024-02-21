3522d20315d5c9267f0f01dd44aeeca68bd9fec5
iamprashuva <prashuvadhital@gmail.com>
2024-02-21 15:47:01 +0545

    Completed Assignment1

--- Assignment1/Greeter/Program.cs
+++ Assignment1/Greeter/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+
 var fullName = "Prashuva Dhital";
 Console.WriteLine(fullName);
 
+// Name to upper Case
 var cFullName = fullName.ToUpper();
 Console.WriteLine($"Hello,{cFullName}!");
 
 //Ask user to enter the name
 Console.WriteLine("Please enter your full name:");
 var FullName = Console.ReadLine();
+Console.WriteLine($"Hello, {FullName}");
+
 
 CalculateAge a = new();
 a.Age();
