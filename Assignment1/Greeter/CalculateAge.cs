fb48e65eb3af3fb8b7beaad940165d50175efe81
iamprashuva <prashuvadhital@gmail.com>
2024-02-25 20:51:51 +0545

    Calculated age

--- Assignment1/Greeter/CalculateAge.cs
+++ Assignment1/Greeter/CalculateAge.cs
@@ -2,47 +2,21 @@ class CalculateAge
 {
     public void Age()
     {
-        // Ask user to enter DOB
-        Console.WriteLine("Please enter your DOB(yyyy/MM/dd)");
-        string? DOB = Console.ReadLine();
+        Console.WriteLine("Enter your Date of Birth as (YYYY/MM/DD):");
+        DateTime DOB = DateTime.Parse(Console.ReadLine());
+        DateTime today = DateTime.Today;
 
-        // parse the user input DOB to DateTime Object
-        if (DateTime.TryParse(DOB, out DateTime Dob))
-        {
-            string formattedDob = Dob.ToString("dddd, dd MMMM yyyy");
-            Console.WriteLine($"Your DOB : {formattedDob}");
+        TimeSpan age = today - DOB;
+        int totalDays = Convert.ToInt32(age.TotalDays);
+        int totalHours = Convert.ToInt32(age.TotalHours);
+        int totalMinutes = Convert.ToInt32(age.TotalMinutes);
 
-            DateTime today = DateTime.Today;
-            int ageInYears = today.Year - Dob.Year;
-            int ageInMonths = today.Month - Dob.Month;
-            int ageInDays = today.Day - Dob.Day;
+        int years = totalDays / 365;
+        int months = (totalDays % 365) / 30;
+        int weeks = (totalDays % 365 % 30) / 7;
+        int days = totalDays % 365 % 30 % 7;
 
-            // Adjust negative values
-            if (ageInMonths < 0 || (ageInMonths == 0 && ageInDays < 0))
-            {
-                ageInYears--;
-                ageInMonths += 12;
-            }
-            if (ageInDays < 0)
-            {
-                ageInMonths--;
-                if (ageInMonths < 0)
-                {
-                    ageInYears--;
-                    ageInMonths += 12;
-                }
-                ageInDays += DateTime.DaysInMonth(today.Year, today.Month);
-            }
-
-            // Calculate age in weeks and remaining days
-            int ageInWeeks = ageInDays / 7;
-            ageInDays %= 7;
-
-            Console.WriteLine($"Age as of now: {ageInYears} years, {ageInMonths} months, {ageInWeeks} weeks, {ageInDays} days");
-        }
-         else
-        {
-            Console.WriteLine("Invalid date format. Please enter the date in the format yyyy/MM/dd.");
-        }
+        Console.WriteLine($"Your DOB is {DOB.ToString("dddd, dd MMMM yyyy")}.");
+        Console.WriteLine($"You are {years} years {months} months {weeks} weeks and {days} days old. \n");
     }
 }
