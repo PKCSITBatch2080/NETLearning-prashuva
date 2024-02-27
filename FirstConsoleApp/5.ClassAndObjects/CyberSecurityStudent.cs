56e08d7f444f0095ee0bc22b12f2395f4a82735c
iamprashuva <prashuvadhital@gmail.com>
2024-02-27 08:14:29 +0545

    Multi-level inheritance

---
+++ FirstConsoleApp/5.ClassAndObjects/CyberSecurityStudent.cs
@@ -0,0 +1,19 @@
+//Multi-Level inheritance
+class CyberSecurityStudent : CSStudent
+{
+    public CyberSecurityStudent(string name, byte rn, string pTitle, string certification)
+        : base(name, rn, pTitle)
+    {
+        SecurityCertification = certification;
+    }
+    public string EthicalHackingCourseTitle {get; set;}
+    public string SecurityCertification { get; set; }
+
+    public DateTime CertifiedDate { get; set; }
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Certification:{SecurityCertification}");
+        Console.WriteLine($"Certified date:{CertifiedDate }");
+    }
+}
