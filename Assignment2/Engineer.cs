9071460fbe4619b7e1ed6ae94de555c5deb239da
iamprashuva <prashuvadhital@gmail.com>
2024-03-03 12:23:04 +0545

    Engineer subclass(Multiple Inheritance)

---
+++ Assignment2/Engineer.cs
@@ -0,0 +1,26 @@
+//Multiple Inheritance
+public class Engineer : Employee, IMeetingAttendee
+{
+    public string ProgrammingLanguage { get; set; }
+
+    public Engineer(int employeeID, int hireYear, string programmingLanguage) : base(employeeID, hireYear)
+    {
+        ProgrammingLanguage = programmingLanguage;
+    }
+ // New method specific to Engineer, can be overridden
+    public virtual void SolveProblem()
+    {
+        Console.WriteLine("Engineer is solving a problem.");
+    }
+
+    public override void Work()
+    {
+        Console.WriteLine("Engineer is coding.");
+    }
+
+    // Implementing interface method
+    public void AttendMeeting()
+    {
+        Console.WriteLine("Engineer is attending a meeting.");
+    }
+}
