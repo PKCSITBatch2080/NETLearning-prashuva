5669c06e76ad33bcb8853eb385387906da05623e
iamprashuva <prashuvadhital@gmail.com>
2024-03-03 12:23:32 +0545

    inherited Engineer SubClass

---
+++ Assignment2/TeamLead.cs
@@ -0,0 +1,32 @@
+//Multi-level inheritance
+public class TeamLead : Engineer
+{
+    private static int teamLeadCount;
+
+     public TeamLead(int employeeID, int hireYear, string programmingLanguage) : base(employeeID, hireYear, programmingLanguage)
+    {
+        teamLeadCount++;
+    }
+
+    public static int TeamLeadCount
+    {
+        get { return teamLeadCount; }
+    }
+
+    public void LeadTeam()
+    {
+        Console.WriteLine("Team Lead is leading the team.");
+    }
+
+    // Override Engineer's SolveProblem method
+    public override void SolveProblem()
+    {
+        Console.WriteLine("Team Lead is solving a problem while leading the team.");
+    }
+
+    public override void Work()
+    {
+        Console.WriteLine("Team Lead is managing tasks and leading the team.");
+    }
+}
+
