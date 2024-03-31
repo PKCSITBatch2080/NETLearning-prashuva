cb40dd4b83b1dbf70eaa66645edae74e82152e14
iamprashuva <prashuvadhital@gmail.com>
2024-03-31 08:33:19 +0545

    changes made

--- CollegeManager/Program.cs
+++ CollegeManager/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Program}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
